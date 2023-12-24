f2f98715ea95a234b54d19054874ee95de2e015f
Filipe Cardoso <up202006409@up.pt>
2023-12-24 12:08:55 +0000

    Changed Stack from a new algebraic type to a new type (as an alias to the list of StackElement); Renamed empty to createEmptyStack in order to fit the delivery naming requirements

--- stack.hs
+++ stack.hs
@@ -4,40 +4,39 @@ module Stack (Stack , pushInt, pushBool, pop, top, empty, isEmpty, size, fromSta
 data StackElement = StackInt Int | StackString String
     deriving (Show, Eq, Ord)
 
-data Stack = Stk [StackElement]
-    deriving (Show)
+type Stack = [StackElement]
 
 -- Pushes an Int/Integer into the Stack 
 pushInt :: Integral a => a -> Stack -> Stack
-pushInt num (Stk xs) = Stk (StackInt (fromIntegral num) : xs)
+pushInt num xs = StackInt (fromIntegral num) : xs
 
 -- Pushes a String into the Stack
 pushBool :: Bool -> Stack -> Stack
-pushBool True (Stk xs) = Stk (StackString "tt" : xs)
-pushBool False (Stk xs) = Stk (StackString "ff" : xs)
+pushBool True xs = StackString "tt" : xs
+pushBool False xs = StackString "ff" : xs
 
 -- Gets the top element of the Stack 
 top :: Stack -> StackElement
-top (Stk (x:_)) = x
+top (x:_) = x
 top _ = error "Stack.top: Stack is Empty"
 
 -- Verifies if the Stack is empty 
 isEmpty :: Stack -> Bool 
-isEmpty (Stk (x:_)) = False 
+isEmpty (x:_) = False 
 isEmpty _ = True 
 
 -- Generates a empty Stack 
-empty :: Stack 
-empty = Stk []
+createEmptyStack :: Stack 
+createEmptyStack = []
 
 -- Pops an element out of the Stack 
 pop :: Stack -> Stack
-pop (Stk (_:xs)) = Stk xs
+pop (_:xs) = xs
 pop _ = error "Stack.pop: Stack is empty"
 
 -- Gets the ammount of elements inside the Stack 
 size :: Stack -> Int
-size (Stk(xs)) = length xs
+size (xs) = length xs
 
 -- Converts a StackElementString back to String
 fromStackElementInt :: StackElement -> Int 
